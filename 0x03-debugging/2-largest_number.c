580645716bab38989a8106f71b8887cb72fe7bf3
aminuv <mohamed.elmarbouh07@gmail.com>
2023-03-22 14:37:04 -0700

    tasks 03

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,30 +1,66 @@
 #include "main.h"
 /**
-* largest_number - returns the largest of 3 numbers
-* @a: first integer
-* @b: second integer
-* @c: third integer
-* Return: largest number
-*/
-int largest_number(int a, int b, int c)
-{
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
-}
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
+	int largest_number(int a, int b, int c);
+
+/**
+ * largest_number - return max
+ * @a: the first number
+ * @b: the second number
+ * @c: the third number
+ * Return: the largest number
+ */
+
+int largest_number(int a, int b, int c)
+	{
+		int largest;
+
+		if (a > b && a > c)
+		{
+			largest = a;
+		}
+		else if (b > a && b > c)
+		{
+			largest = b;
+		}
+		else if (c > a && c > b)
+		{
+			largest = c;
+		}
+		else if (a == b && a > c)
+		{
+			largest = a;
+		}
+		else if (a == c && a > b)
+		{
+			largest = a;
+		}
+		else if (b == c && b > a)
+		{
+			largest = b;
+		}
+		else if (b == a && b > c)
+		{
+			largest = b;
+		}
+		else if (c == a && c > b)
+		{
+			largest = c;
+		}
+		else if (c == b && c > a)
+		{
+			largest = c;
+		}
+		else
+		{
+			largest = a = b = c;
+		}
+
+		return (largest);
+	}
