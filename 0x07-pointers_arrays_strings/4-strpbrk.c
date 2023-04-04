ec689de2b6e561bdcc5f66ccce0b58f4afaf02dd
aminuv <mohamed.elmarbouh07@gmail.com>
2023-04-04 08:32:22 -0700

    tasks from 0 to 5

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any set of bytes
+ * @s: input
+ * @accept: input
+ *
+ * Return: 0 
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int q;
+
+		while (s[q] != '\0')
+		{
+			for (q = 0; accept[q]; q++)
+			{
+			if (*s == accept[q])
+			return (s + q);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
