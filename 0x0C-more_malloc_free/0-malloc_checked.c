4814f3c6ba986513162400cb39600918c367e0ac
aminuv <mohamed.elmarbouh07@gmail.com>
2023-04-13 08:15:31 -0700

    tasks of 0x0C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,15 +8,14 @@
  *
  *return: pointer
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+void *ptr;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-	{
-		exit(98);
-	}
-	return (ptr);
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
 }
