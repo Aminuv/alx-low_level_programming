389ad177453052fba7846a2fec06562319bd8f10
aminuv <mohamed.elmarbouh07@gmail.com>
2023-04-17 10:33:08 -0700

    all the tasks of 0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ *init_gog - initializer
+ *@d: the dog to init
+ *@name: the dog's name
+ *@age: the dog's age
+ *@owner: the owner's name
+ *
+ * return: void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
+
+
+}
