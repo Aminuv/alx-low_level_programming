a7753419449da508c53c1fd285e901d210ef3d08
aminuv <mohamed.elmarbouh07@gmail.com>
2023-04-17 10:15:31 -0700

    all the tasks of 0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - it's a dog structr
+ * @name : the name
+ * @age : the age
+ * @owner : tne name of the owner
+ *
+ * Description : longer
+ */
+
+struct dog
+{
+	char *name, *owner;
+	float *age;
+};
+#endif
+
