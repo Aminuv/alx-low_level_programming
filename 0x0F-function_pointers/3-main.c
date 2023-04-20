f4aea18ff88fe24e9b5e2afae624f6dc61215c17
aminuv <mohamed.elmarbouh07@gmail.com>
2023-04-20 05:34:10 -0700

    tasks 03

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,55 +1,45 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_98(int elem)
+* main - Prints the result of simple operations
+* @argc: The number of arguments
+* @argv: An array of pointers
+*
+* Return: 0
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    return (98 == elem);
+int num1, num2;
+char *op;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
 }
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+
+if (get_op_func(op) == NULL || op[1] != '\0')
 {
-    return (elem > 0);
+printf("Error\n");
+exit(99);
 }
 
-
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
 {
-    return (elem == 98 || -elem == 98);
+printf("Error\n");
+exit(100);
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
+printf("%d\n", get_op_func(op)(num1, num2));
 
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
+return (0);
 }
