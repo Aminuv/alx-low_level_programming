155abd9a2b2577b8f6cc3ff2dbcbb8d23b8dc54f
aminuv <mohamed.elmarbouh07@gmail.com>
2023-04-27 09:33:08 -0700

    tasksof 0x12 -c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * _strlen - the return of the string
+ *@s: length to checker
+
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - print the list
+ * @h: the  head of the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+	printf("[%d] %s\n" , _strlen(h->str), h->str ? h->str : "(nil)");
+	h = h->next;
+	i++;
+	}
+	return (i);
+}
