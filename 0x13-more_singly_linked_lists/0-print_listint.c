82a3b18e61182f8fb9c3f06ccb7e737e16f15421
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-01 02:07:11 -0700

    tasks 0 and 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- * print_listint - Th Function that prints all the elements of a listint_t list.
+ * print_listint - Th Function that prints all the elements of a listint_t list
  * @h: the head node of the list.
  * Return: Number of nodes.
  */
 
 size_t print_listint(const listint_t *h)
 {
-    size_t cnt = 0;
+	size_t cnt = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-	cnt++;
-    }
-
-    return cnt;
+	while (h != NULL)
+	{printf("%d\n", h->n);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - The function that returns the number of elements in a linked listint_t.
+ * listint_len - A Function returns the number of elements in a linked listint .
  * @h: The head node.
  *
  * Return: Number of elements.
@@ -9,12 +9,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-    size_t cnt = 0;
+	size_t cnt = 0;
 
-    while (h != NULL)
-    {
-        cnt++;
-        h = h->next;
-    }
-    return (cnt);
+	while (h != NULL)
+	{
+		cnt++;
+		h = h->next;
+	}
+	return (cnt);
 }
