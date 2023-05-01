99893f33e86272bebaea9b8e55d7313385644476
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-01 07:31:26 -0700

    tasks 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free the list
+ * @head: the head of list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *i;
+	{
+	while (head)
+		i = head;
+	head = head->next;
+		free(i);
+	}
+}
