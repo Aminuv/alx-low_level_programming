4f0f2b0f39c75240f49ddb12a76f4f58b18788a5
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-01 10:09:30 -0700

    tasks 5

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,2 +1,21 @@
 #include "lists.h"
 
+/**
+ * pop_listint - the head' pop of the node.
+ * @head: pointer to the first node.
+ *
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return;
+
+	node = (*head->next);
+	n = (*head)->n;
+	*head = node;
+	return (n);
+}
