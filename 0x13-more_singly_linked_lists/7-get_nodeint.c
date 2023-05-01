5ce5401e82ce213ed452f17a155c6bd04aaa9740
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-01 12:05:26 -0700

    tasks 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	node = head;
-	while (i < index && head != NULL)
+	while (i < index && node != NULL)
 	{
 		node = node->next;
 		i++;
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * sum_listint - the sum of all data.
+ * @head: pointer to the first node.
+ *
+ * Return: The sum.
+ */
+
+int sum_listint(listint_t *head);
