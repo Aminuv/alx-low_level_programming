19f91285a759cb06bc07547ca45235e05319bff0
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-02 09:58:19 -0700

    tasks 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,11 +27,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 	node = *head;
-	for (i = 0; i == idx - 1  && new_node != NULL ; i++)
+	for (i = 0; i == idx - 1 && new_node != NULL; i++)
 	{
-			new_node->next = node->next;
-			node->next = new_node;
-			return (new_node);
+		new_node->next = node->next;
+		node->next = new_node;
+		return (new_node);
 	}
 			node = node->next;
 
