41cb2c4dd636384b5960c406ece65fea92926bfb
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-04 10:09:53 -0700

    tasks

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets a value of bit
+ * @n: pointe to bit
+ * @index: index of bit
+ *
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
