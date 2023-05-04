8d93922d161e67cf1a7fb033815cf085e063743c
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-04 10:34:04 -0700

    tasks

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int i = 0;
 	unsigned long int s = n ^ m;
 
-	for (; s != 0; i +=1)
+	for (; s != 0; i += 1)
 		s &= (s - 1);
 	return (i);
 }
