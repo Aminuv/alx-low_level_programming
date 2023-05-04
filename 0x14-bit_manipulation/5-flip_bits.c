72079902a5433c5e61b69c331c69d2e260e7c713
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-04 10:32:33 -0700

    tasks

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - function return the number of bits
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i = 0;
+	unsigned long int s = n ^ m;
+
+	for (; s != 0; i +=1)
+		s &= (s - 1);
+	return (i);
+}
