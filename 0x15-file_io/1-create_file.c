229536dd76341d67cb1384fb09aa3354d0e86262
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-08 09:08:41 -0700

    tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || of == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
 		for (text = 0; text_content[text];)
 			text++;
