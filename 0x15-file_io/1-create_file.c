d124667b8390b38b6a32a7aa172e123def69f40a
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-08 09:03:27 -0700

    tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - Function creates a file.
+ * @filename: The name of tat file.
+ * @text_content:is a pointer of straing.
+ *
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int of, bw, text;
+
+	of = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (filename == NULL || of == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (text = 0; text_content[text];)
+			text++;
+	}
+
+	 bw = write(of, text_content, text);
+	 if (bw == -1)
+		 return (-1);
+
+	close(of);
+	return (1);
+}
