d53c62c4d1274c326eb6d7ee74c1a7d4e957785e
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-08 10:12:58 -0700

    tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	 of = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
+	 of = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (of == -1)
 		return (-1);
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,12 @@
 #include "main.h"
 
 /**
- * append_text_to_file - 
+ * append_text_to_file - appends text at the end of the file.
+ * @filname: the name of file.
+ * @text_content: pointer to string.
+ *
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
