72038dbecc0657345675a9c38107a69a2806b574
aminuv <mohamed.elmarbouh07@gmail.com>
2023-05-08 11:12:17 -0700

    tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,14 +16,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL || of == -1)
 		return (-1);
 
+	text = 0;
 	if (text_content != NULL)
 	{
 		while (text_content[text] != '\0')
 			text++;
 	}
-
 	bw = write(of, text_content, text);
-
 	close(of);
 	
 	if (bw == -1)
