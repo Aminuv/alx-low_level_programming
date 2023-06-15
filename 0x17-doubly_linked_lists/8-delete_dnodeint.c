68474d1dd0bf0ac5df79557aa319e79a7529a951
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-15 10:50:26 -0700

    tasks 08

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,9 +31,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		i++;
 	}
 	current->prev->next = current->next;
+
 	if (current->next != NULL)
 		current->next->prev = current->prev;
 	free(current);
-	return (-1);
+	return (1);
 
 }
