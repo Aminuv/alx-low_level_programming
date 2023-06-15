f9336f86a2ad99867712ac5bcfdbe0bcf41424b2
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-15 10:40:25 -0700

    tasks 08

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to a pointer
+ * @idx: index of the list where new node will be added
+ * @n:the element to be added
+ *
+ * Return: address of new node || NULL on failure
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - Function Deletes the node at the given index
+ * @head: Pointer to the address
+ * @index: Index
+ *
+ * Return: 1 , -1 if it failed.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return(-1);
+
+	current = *head;
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (-1);
+
+}
