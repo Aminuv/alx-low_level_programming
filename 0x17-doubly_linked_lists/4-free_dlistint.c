c3ec8b8088642c75c155bfe669ccd6167dce24dd
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-15 08:43:37 -0700

    tasks 04

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - a function that frees the dlistint lst
+ * @head: pointer to that head 
+ *
+ * Return: 0
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *stm;
+
+	while (head)
+	{
+		stm = head;
+		head = head->next;
+		free(stm);
+	}
+}
