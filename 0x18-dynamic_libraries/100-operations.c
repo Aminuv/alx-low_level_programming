b01c11d3f7bbd5b505ed3473f55ce3d3d08b661c
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-06-19 11:56:31 -0700

    tasks100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * add - add a num
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub a num
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplai a num
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divis a num
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modul a num
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
