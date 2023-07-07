051ebbb7130d9d1b54d1dc626ac73b65653cb436
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-07 09:37:15 -0700

    tasks 06

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,24 +6,26 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *node, *te;
-	unsigned long int cont = 0;
+	hash_node_t *cur, *te;
+	unsigned long int cont;
 
 	if (ht == NULL)
 		return;
 
 	for (cont = 0; cont < ht->size; cont++)
 	{
-		node = ht->array[cont];
-		while (node != NULL)
+		cur = ht->array[cont];
+
+		while (cur != NULL)
 		{
-			te = node;
-			node = node->next;
-			free(te->key);
-			free(te->value);
-			free(te);
+			te = cur->next;
+			free(cur->key);
+			free(cur->value);
+			free(cur);
+			cur = te;
 		}
 	}
 	free(ht->array);
+	ht->size = 0;
 	free(ht);
 }
