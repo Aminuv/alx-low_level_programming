a3606b9e1cad837b9d0c8251a86fc6e40c3420e9
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-07-07 07:32:50 -0700

    tasks 04

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,23 +10,23 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *_node;
+	hash_node_t *node;
 	unsigned long int index;
 
 	if (ht == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
-	_node = ht->array[index];
+	node = ht->array[index];
 
-	if (_node == NULL)
+	if (node == NULL)
 		return (NULL);
 
-	while (_node != NULL)
+	while (node != NULL)
 	{
-		if (!strcmp(_node->key, key))
-			return (_node->value);
-		_node = _node->next;
+		if (!strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
 	}
 	return (NULL);
 }
