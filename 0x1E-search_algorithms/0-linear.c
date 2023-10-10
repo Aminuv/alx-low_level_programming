76113e46d094da1389afa19e374f7633a9a76dc4
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-10 12:32:46 -0700

    tasks 00

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+ /**
+  * linear_search - searches for a value in an array.
+  * @array: the input of the array
+  * @size: the size of it 
+  * @value: a value of searsh
+  *
+  *Return: index
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
+
+}
