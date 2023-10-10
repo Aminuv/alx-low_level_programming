38645138a9dc2d495f0b610b11e19e6150fa547f
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-10 13:21:28 -0700

    tasks 01

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted an array
+ * @array: pointer to array
+ * @size: The number of element
+ * @value: a value of search
+ *
+ * Return: -1 if not found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t x, r, l;
+
+	if (!array)
+		return (-1);
+
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		printf ("Searching in array: ");
+		for (x = l; x < r; x++)
+			printf ("%d, ", array[x]);
+		printf("%d\n", array[x]);
+
+		x = l + (r - l) /2;
+		if (array[x] == value)
+			return (x);
+		if (array[x] > value)
+			r = i -1;
+		else
+			l = x + 1;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -17,3 +17,4 @@ typedef struct listint_s
 } listint_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value)
