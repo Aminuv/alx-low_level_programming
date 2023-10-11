95b33dd8a6e74e77df2c3afa6e01044b651e6851
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-11 12:52:51 -0700

    Fix tasks 00

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
 	for (x = 0; x < size; x++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+
 		if (array[x] == value)
 			return (x);
 	}
 	return (-1);
-
 }
